94e71013a6da6daa8edcbb487e82033962dc0853
thedarksideofjapan <juniorsmgks@gmail.com>
2025-09-04 14:39:56 -0300

    update

--- core/views.py
+++ core/views.py
@@ -6,18 +6,18 @@ def custom_404_view(request, exception):
     """
     View personalizada para erro 404 - Página não encontrada
     """
-    return HttpResponseNotFound(render(request, '404.html'))
+    return HttpResponseNotFound(render(request, 'errors/404.html'))
 
 
 def custom_500_view(request):
     """
     View personalizada para erro 500 - Erro interno do servidor
     """
-    return HttpResponseServerError(render(request, '500.html'))
+    return HttpResponseServerError(render(request, 'errors/500.html'))
 
 
 def custom_403_view(request, exception):
     """
     View personalizada para erro 403 - Acesso negado
     """
-    return HttpResponseForbidden(render(request, '403.html')) 
+    return HttpResponseForbidden(render(request, 'errors/403.html'))
