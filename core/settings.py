b39b953ad6e8ba21ef105bb359a794f64e8a2be7
thedarksideofjapan <juniorsmgks@gmail.com>
2025-09-03 23:41:04 -0300

    update mails

--- core/settings.py
+++ core/settings.py
@@ -238,6 +238,8 @@ else:
     EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
     EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
     EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+    DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='no-reply@nexiun.com.br')
+    SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
 MESSAGES_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
